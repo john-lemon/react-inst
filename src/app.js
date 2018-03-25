3c24dc498bb43cf28b0243d3931b7f1a54b35dd3
Roman Dmiterko <dmiterko35@gmail.com>
2018-03-25 21:51:43 +0300

    Changed instagram user in example

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import Index from './Index'
 
 ReactDOM.render(
   <div>
-    <Index user='rm_dm' picsPerPage={6} />
+    <Index user='damedvedev' picsPerPage={6} />
     <Index location='275099403' picsPerPage={6} />
     <Index tag='kitty' picsPerPage={6} />
   </div>, document.getElementById('app')
